3e00333b7c082ec5de9801ad2e556f42b696727d
German <gtumorin@gmail.com>
2022-12-22 19:50:16 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useImmer } from "use-immer";
+import getModal from "./modals/index.js";
+import { Button } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const [isActiveAddForm, setAddFormState] = useState(false);
+  const [tasks, addTask] = useState([]);
+  const Add = getModal("adding");
+
+  const clickHandle = () => {
+    setAddFormState(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="mb-3">
+        <Button
+          onClick={clickHandle}
+          variant="dark"
+          type="button"
+          data-testid="item-add"
+          className="btn btn-secondary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          add new Task
+        </Button>
+      </div>
+      {tasks.length === 0 ? null : (
+        <div>
+          <span className="mr-3">first task!</span>
+          <Button
+            type="button"
+            className="border-0 btn btn-link mr-3 text-decoration-none"
+            data-testid="item-rename"
+          >
+            rename
+          </Button>
+          <Button
+            type="button"
+            className="border-0 btn btn-link text-decoration-none"
+            data-testid="item-remove"
+          >
+            remove
+          </Button>
+        </div>
+      )}
+      {isActiveAddForm ? (
+        <Add
+          tasks={tasks}
+          addTask={addTask}
+          setAddFormState={setAddFormState}
+        />
+      ) : null}
+    </>
   );
-}
+};
 
 export default App;
